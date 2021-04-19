e1d667c310085d8d2c760572c5e003098b9549bd
rahman-ashik <49172869+rahman-ashik@users.noreply.github.com>
2021-04-18 23:03:13 -0400

    ActionBar Removal and Login-Register transition added
    1. ActionBars from LoginActivity and RegisterActivity removed.
    2. onClick listeners were added to transit between LoginActivity and RegisterActivity

--- app/src/main/java/com/example/deelio/LoginActivity.java
+++ app/src/main/java/com/example/deelio/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.deelio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    //Declare all elements that will need probable coding
+    TextView tvGoToRegisterActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.getSupportActionBar().hide();
+
+        //list all element bindings here
+        tvGoToRegisterActivity= (TextView)findViewById(R.id.loginNregisterSwitcher);
+
+
+
+        tvGoToRegisterActivity.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
+
 }
