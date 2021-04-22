bb0eaf88ba64de1e7064a26db2f55644cf946b86
rahman-ashik <49172869+rahman-ashik@users.noreply.github.com>
2021-04-22 17:03:02 -0400

    bottom Nav instance

--- app/src/main/java/com/example/deelio/MainActivity.java
+++ app/src/main/java/com/example/deelio/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.deelio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.example.deelio.Fragments.HomeFragment;
+import com.example.deelio.Fragments.PostFragment;
+import com.example.deelio.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
@@ -21,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
         final Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
-
     }
+
+    //define fragments and manage
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    final Fragment homeFragment = new HomeFragment();
+    final Fragment postFragment = new PostFragment();
+    final Fragment profileFragment = new ProfileFragment();
+
+    //get bottomNav referrence
+//    BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+
+
+
+
+
+
+
 }
